2aa6af0dcb51d0397bb78ce9e73b74c827f6d224
mrOrange1337 <51816723+mrOrange1337@users.noreply.github.com>
2019-06-14 14:19:12 +0400

    bootstrap added

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?
 require ("dbConnect.php");
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8"/>
+	<title>Добавление городов</title>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="bootstrap.css" type="text/css" />
+</head>
+<body>
+
+
+<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript" src="bootstrap.js.js"></script>
+</body>
+</html>
