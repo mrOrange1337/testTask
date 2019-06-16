d7431752fb8ba393c0bfed933e24b5f59d9facf5
mrOrange1337 <51816723+mrOrange1337@users.noreply.github.com>
2019-06-16 14:12:29 +0400

    Bootstrap table display added

--- index.php
+++ index.php
@@ -5,8 +5,26 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/header.php';
 $countries = getCountriesFromDB();
 ?>
 
-
-
+<div class="wrapper">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Название страны</th>
+    </tr>
+	</thead>
+  <tbody>
+  <?
+  foreach($countries as $country)
+  {		echo "<tr>";
+	  echo "<td>".$country['id']."</td>";
+	  echo "<td>".$country['countryName']."</td>";
+	  echo "</tr>";
+  }
+  ?>
+  	</tbody>
+	</table>
+</div>
 
 <?
 include_once $_SERVER['DOCUMENT_ROOT'].'/footer.php';
