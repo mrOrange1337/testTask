ce1f55e81f22b4b7e583ea1ac340e4ba6899091a
mrOrange1337 <51816723+mrOrange1337@users.noreply.github.com>
2019-06-17 09:45:07 +0400

    some fixes

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="script.js"></script>
 <script type="text/javascript" src="bootstrap.js.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8"/>
 	<title>Добавление городов</title>
-	<link rel="stylesheet" href="style.css" type="text/css" />
+	<link rel="stylesheet" href="styles.css" type="text/css" />
 	<link rel="stylesheet" href="bootstrap.css" type="text/css" />
 </head>
 <body>
